46c51d862672d52c3b4a07cace634d7b39936c0f
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-03-13 11:59:59 -0700

    Converted to TypeScript

---
+++ src/ArcGisHelper.ts
@@ -0,0 +1,204 @@
+import DrawUI = require("./ArcGisDrawUI");
+import declare = require("dojo/_base/declare");
+import lang = require("dojo/_base/lang");
+import Evented = require("dojo/Evented");
+import Draw = require("esri/toolbars/draw");
+import GraphicsLayer = require("esri/layers/GraphicsLayer");
+import Graphic = require("esri/graphic");
+
+/**
+ * @typedef {Object} SymbolOptionsOptions
+ * @property {esri/symbols/MarkerSymbol} point - Symbol that will be used for point features.
+ * @property {esri/symbols/LineSymbol} line - Symbol that will be used for line features.
+ * @property {esri/symbols/FillSymbol} polygon - Symbol that will be used for polygon features.
+ * @property {esry/symbols/TextSymbol} text - Symbol that will be used as the basis for text labels.
+ */
+
+/**
+ * Contains point, line, and polygon symbols.
+ * @param {(esri/symbols/MarkerSymbol|SymbolOptionsOptions)} pointSymbolOrSymbolOptions
+ * @param {esri/symbols/LineSymbol} [lineSymbol] - Required if first parameter was a MarkerSymbol.
+ * @param {esri/symbols/FillSymbol} [polygonSymbol] - Required if the first parameter was a MarkerSymbol.
+ * @param {esry/symbols/TextSymbol} [textSymbol] - Required if the first parameter was a MarkerSymbol
+ */
+function SymbolOptions(...symbols: any[]) {
+  if (arguments.length === 1) {
+    Object.defineProperties(this, {
+      point: { value: arguments[0].point },
+      line: { value: arguments[0].line },
+      polygon: { value: arguments[0].polygon },
+      text: { value: arguments[0].text }
+    });
+  } else if (arguments.length === 4) {
+    Object.defineProperties(this, {
+      point: { value: arguments[0] },
+      line: { value: arguments[1] },
+      polygon: { value: arguments[2] },
+      text: { value: arguments[3] }
+    });
+  } else {
+    throw TypeError("Invalid number of arguments.");
+  }
+}
+
+/**
+ * Get's the symbol approprate for the graphic.
+ * @param {(esri/Graphic|esri/geometries/Geometry)} graphicOrGeometry
+ * @returns {esri/symbols/Symbol}
+ */
+SymbolOptions.prototype.getSymbol = function(graphicOrGeometry) {
+  let geometry, symbol;
+  if (graphicOrGeometry) {
+    if (graphicOrGeometry.geometry) {
+      geometry = graphicOrGeometry.geometry;
+    } else if (graphicOrGeometry.type) {
+      geometry = graphicOrGeometry;
+    }
+  }
+  if (geometry) {
+    symbol = /\w*point/i.test(geometry.type)
+      ? this.point
+      : /\w*line/i.test(geometry.type) ? this.line : this.polygon;
+  }
+  return symbol;
+};
+
+/**
+ * Creates a DrawUI and esri/toolbars/draw and then connects them.
+ */
+const DrawUIHelper = declare(undefined, [Evented], {
+  layer: null,
+  draw: null,
+  symbolOptions: null,
+  /**
+   *
+   * @param {esri/Map} map
+   * @param {HTMLElement} drawUIElement
+   * @param {SymbolOptions} [symbols]
+   * @param {Object} graphicsLayerOptions - See esri/layer/GraphicsLayer constructor for details.
+   * @constructs
+   */
+  constructor(map, drawUIElement, symbols, graphicsLayerOptions) {
+    const self = this;
+
+    /**
+     * Create a text symbol IF the text button is currently active; otherwise returns null.
+     * @returns {?esri/symbols/TextSymbol}
+     */
+    function createTextSymbol() {
+      const activeTextButton = drawUIElement.querySelector(
+        "button.text.active"
+      );
+      // TODO: get text from a UI control.
+      let output = null;
+      if (activeTextButton) {
+        output = lang.clone(symbols.text);
+        output.setText(
+          drawUIElement.querySelector("input[name='label']").value
+        );
+      }
+      return output;
+    }
+
+    /**
+     * Removes the "active" class from the buttons.
+     */
+    function removeActiveStyling() {
+      const activeButtons = drawUIElement.querySelectorAll("button.active");
+
+      for (let i = 0, l = activeButtons.length; i < l; i += 1) {
+        activeButtons[i].classList.remove("active");
+      }
+    }
+
+    /**
+     * @this {HTMLButtonElement}
+     */
+    const handleButtonClick = function() {
+      const operation = this.value;
+      if (operation) {
+        if (operation === "CLEAR") {
+          if (
+            window.confirm(
+              "Are you sure you want to delete all of your drawn features?"
+            )
+          ) {
+            gLayer.clear();
+          }
+        } else {
+          map.setInfoWindowOnClick(false);
+          if (operation === "TEXT") {
+            draw.activate(Draw.POINT);
+            self.emit("draw-activate", {});
+          } else {
+            draw.activate(Draw[operation]);
+            self.emit("draw-activate", {});
+          }
+          // Add a class to allow "active" operation's button to by styled.
+          removeActiveStyling();
+          this.classList.add("active");
+        }
+      }
+    };
+
+    // Setup default graphics layer options for undefined.
+    if (!graphicsLayerOptions) {
+      graphicsLayerOptions = {};
+    }
+
+    if (symbols && !(symbols instanceof SymbolOptions)) {
+      symbols = new SymbolOptions(symbols);
+    }
+
+    this.symbols = symbols || null;
+
+    graphicsLayerOptions.id = graphicsLayerOptions.id || "drawnFeatures";
+    graphicsLayerOptions.styling = this.symbols
+      ? true
+      : Boolean(graphicsLayerOptions.styling);
+    graphicsLayerOptions.className =
+      graphicsLayerOptions.className || "drawn-features";
+    graphicsLayerOptions.dataAttributes =
+      graphicsLayerOptions.dataAttributes || "geometry-type";
+
+    // Create a graphics layer to store drawn graphics.
+    const gLayer = new GraphicsLayer(graphicsLayerOptions);
+
+    this.layer = gLayer;
+
+    map.addLayer(gLayer);
+
+    // Create the Draw toolbar.
+    const draw = new Draw(map);
+    this.draw = draw;
+
+    // Setup the Draw toolbar's event handler that will add graphics to the graphics layer.
+    draw.on("draw-complete", function(e) {
+      this.deactivate();
+      self.emit("draw-complete", e);
+      map.setInfoWindowOnClick(true);
+      const graphic = new Graphic(
+        e.geometry,
+        symbols ? createTextSymbol() || symbols.getSymbol(e.geometry) : null,
+        { "geometry-type": e.geometry.type }
+      );
+      gLayer.add(graphic);
+      // Remove styling for active buttons.
+      removeActiveStyling();
+    });
+
+    // Create the Draw UI.
+    const drawUI = new DrawUI(drawUIElement);
+
+    // Assign event handlers to buttons.
+    const buttons = drawUI.root.querySelectorAll("button");
+
+    for (let i = 0, l = buttons.length; i < l; i += 1) {
+      buttons[i].addEventListener("click", handleButtonClick);
+    }
+  }
+});
+
+DrawUIHelper.SymbolOptions = SymbolOptions;
+
+export = DrawUIHelper;
